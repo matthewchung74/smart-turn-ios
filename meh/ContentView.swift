9e0ace2f684ffd4ff837e0b602973cdbd9de1ad4
YHat.pub <matt@yhat.pub>
2025-11-13 12:05:46 -0800

    Initial Commit

---
+++ meh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  meh
+//
+//  Created by Matt Chung on 11/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ meh/mehApp.swift
@@ -0,0 +1,17 @@
+//
+//  mehApp.swift
+//  meh
+//
+//  Created by Matt Chung on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct mehApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
